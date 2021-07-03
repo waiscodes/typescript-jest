6a534584bc67437576b43f9af728bbf210f87fd8
waiscodes <b.wais@outlook.com>
2021-07-02 23:10:03 -0600

    changed book interface into an array and changed it into state as opposed to props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 // components
 import Book from "./components/Books";
 
 // Book Interface
-interface IProps {
-  book: {
+interface IState {
+  books: {
     title: string;
     author: string;
     upvotes: number;
     recBy: string;
-  };
+  }[];
 }
 
 function App() {
+  const [books, setBooks] = useState<IState["books"]>([]);
+
   return (
     <div className='App'>
       <h1>Hello world</h1>
