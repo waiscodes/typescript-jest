bd519f545bdf9f7e0d0866bc8b139103bdcaaeaa
waiscodes <b.wais@outlook.com>
2021-07-02 23:12:30 -0600

    added a prop of interface to books to Book to require it and cause error in app

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const Book = () => {
+// Props Interface
+interface IProps {
+  books: {
+    title: string;
+    author: string;
+    upvotes: number;
+    recBy: string;
+  }[];
+}
+
+const Book = ({ books }: IProps) => {
   return (
     <div>
       <h1>hello world from Book component</h1>
