02f299fcd7551131872124c296c4a5a0d3fc3889
waiscodes <b.wais@outlook.com>
2021-07-02 23:30:37 -0600

    Created a render books method to pass to the return of the component

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -11,6 +11,18 @@ interface IProps {
 }
 
 const Book: React.FC<IProps> = ({ books }) => {
+  const renderBooks = (): JSX.Element[] => {
+    return books.map((book) => {
+      return (
+        <li>
+          <p>{book.title}</p>
+          <p>{book.author}</p>
+          <p>{book.upvotes}</p>
+          <p>{book.recBy}</p>
+        </li>
+      );
+    });
+  };
   return (
     <div>
       <h1>hello world from Book component</h1>
