d330354b234a0d87a478361b9c2461f698891add
waiscodes <b.wais@outlook.com>
2021-07-02 23:03:59 -0600

    imported books into app and rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import React from "react";
 import "./App.css";
+
+// components
+import Books from "./components/Books";
 
 function App() {
   return (
     <div className='App'>
       <h1>Hello world</h1>
+      <Books />
     </div>
   );
 }
