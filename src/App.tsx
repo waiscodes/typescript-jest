22815bb09f297582c49900ae1008e0a0aeb33171
waiscodes <b.wais@outlook.com>
2021-07-04 00:03:53 -0600

    added setblockchain and blockchain states as porps to add block

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Hello world</h1>
-      <AddBlock />
+      <AddBlock setBlockchain={setBlockchain} blockchain={blockchain} />
       <Blockchain blockchain={blockchain} />
     </div>
   );
