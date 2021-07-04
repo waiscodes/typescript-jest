edaf34c97f8477e229c9a71d99f197705feeb90b
waiscodes <b.wais@outlook.com>
2021-07-04 00:00:23 -0600

    added props and interface to add block

--- src/components/AddBlock.tsx
+++ src/components/AddBlock.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { IState as Props } from "../App";
 
-const AddBlock = () => {
+// Block interface
+interface IProps {
+  setBlockchain: React.Dispatch<React.SetStateAction<Props["blockchain"]>>;
+  blockchain: Props["blockchain"];
+}
+
+const AddBlock: React.FC<IProps> = ({ setBlockchain, blockchain }) => {
   return (
     <div>
       <p>Hello world from add block component</p>
