243b8a44136ded741971a14932ee92bcf045c00d
waiscodes <b.wais@outlook.com>
2021-07-07 22:26:45 -0600

    Added an input state to display the input and the hash

--- src/components/Sha256Hash.tsx
+++ src/components/Sha256Hash.tsx
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 import sha256 from "crypto-js/sha256";
 
 const Sha256 = () => {
-  const [hash, setHash] = useState<string>("");
+  const [input, setInput] = useState<string>("Hello World");
+  const [hash, setHash] = useState<string>(sha256("Hello World").toString());
 
   const createHashOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
     setHash(sha256(e.target.value).toString());
   };
 
   return (
     <div>
+      <p>The Hash for "{input}" is: </p>
       <p>{hash}</p>
-      <input type='text' onChange={createHashOnChange} />
+      <input
+        type='text'
+        placeholder='Hello World'
+        onChange={createHashOnChange}
+      />
     </div>
   );
 };
