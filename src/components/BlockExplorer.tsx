015d0b86eb91ca8f1a58f8903185c5430ab67ef5
waiscodes <b.wais@outlook.com>
2021-07-11 13:14:12 -0600

    installed axios, imported it and called block explorer api

--- src/components/BlockExplorer.tsx
+++ src/components/BlockExplorer.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import axios from "axios";
 
 const BlockExplorer = () => {
+  axios.get("https://chain.api.btc.com/v3/block/latest").then((res) => {
+    if (res.data.items) {
+      console.log(res.data.items);
+    }
+  });
+
   return (
     <div>
       <h1>Hello world from the block explorer component</h1>
