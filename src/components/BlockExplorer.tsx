7ef83c5d4e3d460f65155bd9159f30a03ab39d44
waiscodes <b.wais@outlook.com>
2021-07-11 13:08:02 -0600

    created a block explorer component

---
+++ src/components/BlockExplorer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlockExplorer = () => {
+  return (
+    <div>
+      <h1>Hello world from the block explorer component</h1>
+    </div>
+  );
+};
+
+export default BlockExplorer;
