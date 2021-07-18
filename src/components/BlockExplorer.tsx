bfd5541a519dd3c72e191c2e6c3a0615a7f86c1d
waiscodes <b.wais@outlook.com>
2021-07-18 00:45:39 -0600

    Added a chain explorer api key

--- src/components/BlockExplorer.tsx
+++ src/components/BlockExplorer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const BlockExplorer = () => {
-  axios.get("https://chain.api.btc.com/v3/block/latest").then((res) => {
+  axios.get("https://chain.api.btc.com/v3/block/3").then((res) => {
     if (res.data.items) {
       console.log(res.data.items);
     }
